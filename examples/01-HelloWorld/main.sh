a5a47cae758c0f2cdf458997d8c75627564e20c1
Christoph Dorn <christoph@christophdorn.com>
2017-01-25 18:13:54 -0800

    wip

---
+++ examples/01-HelloWorld/main.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash.origin
+eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
+BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
+local __BO_DIR__="$___TMP___"
+
+
+BO_requireModule "${__BO_DIR__}/../../docker.sh" as "localDocker" "localhost"
+
+localDocker list -a
+
+localDocker build "${__BO_DIR__}" "bach.origin.docker.example"
+
+# TODO: Get free port dynamically
+local port="8055"
+localDocker start "bach.origin.docker.example" "${port}"
+localDocker list
+
+# TODO: Instead of just sleeping for 1 second, use curl to call server
+#       until we get a reponse or timeout. Use 'bash.origin.request' to
+#       make the calls.
+sleep 1
+local requestID=`uuidgen`
+local command="curl -s "http://${DOCKER_CONTAINER_HOST_IP}:${port}/?rid=$requestID""
+echo "Command: $command"
+local response=`$command`
+echo "Response: $response"
+if [ "${response}" != "Hello World from dockerized NodeJS process!" ]; then
+		echo "ERROR: Did not get expected response!"
+		exit 1
+fi
+
+localDocker logs "bach.origin.docker.example"
+
+if [[ `localDocker logs "bach.origin.docker.example"` != *"$requestID"* ]]; then
+		echo "ERROR: Did not find request in logs!"
+		exit 1
+fi
+
+localDocker stop "bach.origin.docker.example"
+
+localDocker list
